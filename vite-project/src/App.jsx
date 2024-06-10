ff6e02ed59a7996d1d5f64d42deee88abd3dfe10
Katie Burdett <Katie.Burdett@ao.com>
2024-06-10 15:36:24 +0100

    chore: refactoring to cardData

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import SocialCard1 from "./components/SocialCard1.jsx";
-import "./index.css";
+import CardData from './CardData.jsx';
 
 export default function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch('https://randomuser.me/api?results=10')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setUsers(data.results); // Correctly set the users state
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
   return (
-    <div className="userscontainer">
-      {users.map((user, index) => (
-        <SocialCard1 key={index} userData={user} />
-      ))}
-    </div>
+    <CardData />
   );
 }
--- vite-project/src/CardData.jsx
+++ vite-project/src/CardData.jsx
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import SocialCard1 from './components/SocialCard1';
+import './index.css';
 
-// export default function CardData() {
-//    const [allUsers, setAllUsers] = useState([]);
-//    useEffect(() => {
-//     (async () => {
-//       let userData;
-//       try {
-//         const response = await fetch("https://randomuser.me/api/?results=10");
-//         userData = await response.json();
-//       } catch (error) {
-//         console.log(error);
-//         userData = [];
-//       }
-//       setAllUsers(userData.results);
-//     })();
-//   }, 
-//   []);
-// };
+export function CardData() {
+    const [users, setUsers] = useState([]);
+  
+    useEffect(() => {
+      fetch('https://randomuser.me/api?results=10')
+      .then((response) => response.json())
+      .then((data) => {
+          console.log(data);
+          setUsers(data.results); // Correctly set the users state
+      })
+      .catch((err) => {
+          console.log(err.message);
+      });
+  }, []);
+    
+    return (
+      <div className="userscontainer">
+        {users.map((user, index) => (
+          <SocialCard1 key={index} userData={user} />
+        ))}
+      </div>
+    );
+  }
+  
+  export default CardData;
