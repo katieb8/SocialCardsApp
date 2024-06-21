aca3ddaaeb21ccbf63b66e40abf82fee3a84d696
Katie Burdett <Katie.Burdett@ao.com>
2024-06-21 11:01:47 +0100

    chore: more styling changes

--- vite-project/src/components/SocialCard1.jsx
+++ vite-project/src/components/SocialCard1.jsx
@@ -3,7 +3,11 @@ const SocialCard1 = ({ userData }) => {
         <div className="socialCard1">
             <h2 className="card-title" data-testid={`card-title-${userData.name.first}`}>{userData.name.first}</h2>
             <img className="cardImage" src={userData.picture.medium} alt={`picture of ${userData.name.first}`} data-testid={`profile-pic-${userData.name.first}`}  />
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis illum, dolores autem, neque voluptatum reprehenderit quasi officia cum sapiente sint. Fugiat, facere architecto? Doloribus delectus dolores hic in velit.</p>
+            <div className="card-text-container">
+                <p className="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis illum, dolores autem, neque voluptatum reprehenderit quasi officia cum sapiente sint. Fugiat, facere architecto? Doloribus delectus dolores hic in velit.
+                </p>
+            </div>
         </div>
     )
 };
